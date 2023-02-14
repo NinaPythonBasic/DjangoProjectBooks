245d3594f40f18e2c2eae654e88a2bd403920fa7
NinaPythonBasic <nrodinkova@yandex.ru>
2023-02-14 03:44:28 +0300

    Changed fields' verbose_name to russian language

--- books/models.py
+++ books/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(
-        verbose_name="name", max_length=128, blank=False, null=False
+        verbose_name="название", max_length=128, blank=False, null=False
     )
 
     def __str__(self):
@@ -15,39 +15,42 @@ class Category(models.Model):
 
 class Book(models.Model):
     author = models.CharField(
-        verbose_name="author", max_length=128, blank=False, db_index=True
+        verbose_name="автор", max_length=128, blank=False, db_index=True
     )
     name = models.CharField(
-        verbose_name="name", max_length=256, blank=False, db_index=True
+        verbose_name="название", max_length=256, blank=False, db_index=True
     )
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
-    publisher = models.CharField(verbose_name="publisher", max_length=104)
-    year = models.IntegerField(verbose_name="year of publication")
-    place = models.CharField(verbose_name="place in library", max_length=256)
-    details = models.TextField(verbose_name="details", default="")
-    onhand = models.BooleanField(verbose_name="book on hand", default=False)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name="жанр")
+    publisher = models.CharField(verbose_name="издательство", max_length=104)
+    year = models.IntegerField(verbose_name="год издания")
+    place = models.CharField(verbose_name="место хранения", max_length=256)
+    details = models.TextField(verbose_name="примечание", default="")
+    onhand = models.BooleanField(verbose_name="книга выдана", default=False)
 
     def __str__(self):
         return f"{self.id} {self.name} ({self.author})"
 
+    def __repr__(self):
+        return str(self)
+
 
 class Reader(models.Model):
     name = models.CharField(
-        verbose_name="first name and last name",
+        verbose_name="ФИО",
         max_length=184,
         blank=False,
         db_index=True,
     )
-    address = models.CharField(verbose_name="address", max_length=256)
-    phone = models.CharField(verbose_name="phone", max_length=10)
+    address = models.CharField(verbose_name="адрес", max_length=256)
+    phone = models.CharField(verbose_name="телефон", max_length=10)
 
     def __str__(self):
         return f"{self.id} {self.name}"
 
 
 class BookOnHand(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.PROTECT)
-    reader = models.ForeignKey(Reader, on_delete=models.PROTECT)
-    issuedate = models.DateField(verbose_name="date of issue")
-    returndate = models.DateField(verbose_name="return date", null=True)
-    details = models.TextField(verbose_name="details", default="")
+    book = models.ForeignKey(Book, on_delete=models.PROTECT, verbose_name="книга")
+    reader = models.ForeignKey(Reader, on_delete=models.PROTECT, verbose_name="читатель")
+    issuedate = models.DateField(verbose_name="дата выдачи")
+    returndate = models.DateField(verbose_name="дата возврата", null=True)
+    details = models.TextField(verbose_name="примечание", default="")
