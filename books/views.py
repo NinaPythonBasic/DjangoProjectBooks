e8f153b2dc24a3131010670fadbac012a1acaa55
NinaPythonBasic <nrodinkova@yandex.ru>
2023-02-20 20:54:13 +0300

    Reader (not user.is_authenticated) can search books only

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.db.models import Q
 from django.urls import reverse_lazy
@@ -21,70 +22,73 @@ class BookListView(ListView):
     model = Book
 
     def get_queryset(self):
-        filter_str = self.request.GET.get('q')
+        filter_str = self.request.GET.get("q")
         if filter_str is None:
             result_list = Book.objects.all()
         else:
-            result_list = Book.objects.filter(Q(name__icontains=filter_str) | Q(author__icontains=filter_str))
+            result_list = Book.objects.filter(
+                Q(name__icontains=filter_str) | Q(author__icontains=filter_str)
+            )
         return result_list
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        filter_str = self.request.GET.get('q')
-        context['q'] = filter_str
+        filter_str = self.request.GET.get("q")
+        context["q"] = filter_str
         return context
+
 
 class BookDetailView(DetailView):
     model = Book
 
 
-class BookCreateView(CreateView):
+class BookCreateView(LoginRequiredMixin, CreateView):
     model = Book
     form_class = BookModelForm
     success_url = reverse_lazy("books")
 
 
-class BookUpdateView(UpdateView):
+class BookUpdateView(LoginRequiredMixin, UpdateView):
     model = Book
     form_class = BookModelForm
     success_url = reverse_lazy("books")
 
 
-class BookDeleteView(DeleteView):
+class BookDeleteView(LoginRequiredMixin, DeleteView):
     model = Book
     success_url = reverse_lazy("books")
 
 
-class CategoryListView(ListView):
+class CategoryListView(LoginRequiredMixin, ListView):
     model = Category
 
 
-class CategoryDetailView(DetailView):
+class CategoryDetailView(LoginRequiredMixin, DetailView):
     model = Category
 
 
-class CategoryCreateView(CreateView):
-    model = Category
-    form_class = CategoryModelForm
-    success_url = reverse_lazy("categories")
-
-
-class CategoryUpdateView(UpdateView):
+class CategoryCreateView(LoginRequiredMixin, CreateView):
     model = Category
     form_class = CategoryModelForm
     success_url = reverse_lazy("categories")
 
 
-class CategoryDeleteView(DeleteView):
+class CategoryUpdateView(LoginRequiredMixin, UpdateView):
+    model = Category
+    form_class = CategoryModelForm
+    success_url = reverse_lazy("categories")
+
+
+class CategoryDeleteView(LoginRequiredMixin, DeleteView):
     model = Category
     success_url = reverse_lazy("categories")
 
 
-class ReaderListView(ListView):
+class ReaderListView(LoginRequiredMixin, ListView):
     model = Reader
 
     def get_queryset(self):
-        filter_str = self.request.GET.get('q')
+        filter_str = self.request.GET.get("q")
         if filter_str is None:
             result_list = Reader.objects.all()
         else:
@@ -93,68 +97,72 @@ class ReaderListView(ListView):
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        filter_str = self.request.GET.get('q')
-        context['q'] = filter_str
+        filter_str = self.request.GET.get("q")
+        context["q"] = filter_str
         return context
 
 
-class ReaderDetailView(DetailView):
+class ReaderDetailView(LoginRequiredMixin, DetailView):
     model = Reader
 
 
-class ReaderCreateView(CreateView):
-    model = Reader
-    # fields = "__all__"
-    form_class = ReaderModelForm
-    success_url = reverse_lazy("readers")
-
-
-class ReaderUpdateView(UpdateView):
+class ReaderCreateView(LoginRequiredMixin, CreateView):
     model = Reader
     # fields = "__all__"
     form_class = ReaderModelForm
     success_url = reverse_lazy("readers")
 
 
-class ReaderDeleteView(DeleteView):
+class ReaderUpdateView(LoginRequiredMixin, UpdateView):
+    model = Reader
+    # fields = "__all__"
+    form_class = ReaderModelForm
+    success_url = reverse_lazy("readers")
+
+
+class ReaderDeleteView(LoginRequiredMixin, DeleteView):
     model = Reader
     success_url = reverse_lazy("readers")
 
 
-class BookOnHandListView(ListView):
+class BookOnHandListView(LoginRequiredMixin, ListView):
     model = BookOnHand
 
     def get_queryset(self):
-        filter_str = self.request.GET.get('q')
+        filter_str = self.request.GET.get("q")
         if filter_str is None:
             result_list = BookOnHand.objects.all()
         else:
-            result_list = BookOnHand.objects.filter(Q(book__name__icontains=filter_str) | Q(reader__name__icontains=filter_str))
+            result_list = BookOnHand.objects.filter(
+                Q(book__name__icontains=filter_str)
+                | Q(reader__name__icontains=filter_str)
+            )
         return result_list
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        filter_str = self.request.GET.get('q')
-        context['q'] = filter_str
+        filter_str = self.request.GET.get("q")
+        context["q"] = filter_str
         return context
 
 
-class BookOnHandDetailView(DetailView):
+#
+class BookOnHandDetailView(LoginRequiredMixin, DetailView):
     model = BookOnHand
 
 
-class BookOnHandCreateView(CreateView):
-    model = BookOnHand
-    fields = "__all__"
-    success_url = reverse_lazy("booksonhand")
-
-
-class BookOnHandUpdateView(UpdateView):
+class BookOnHandCreateView(LoginRequiredMixin, CreateView):
     model = BookOnHand
     fields = "__all__"
     success_url = reverse_lazy("booksonhand")
 
 
-class BookOnHandDeleteView(DeleteView):
+class BookOnHandUpdateView(LoginRequiredMixin, UpdateView):
+    model = BookOnHand
+    fields = "__all__"
+    success_url = reverse_lazy("booksonhand")
+
+
+class BookOnHandDeleteView(LoginRequiredMixin, DeleteView):
     model = BookOnHand
     success_url = reverse_lazy("booksonhand")
